9bc1ceaae4bdde6d90a73d84e120d17a08fafd3a
iamr0b0x <abduladeneye@gmail.com>
2022-02-11 11:40:39 +0100

    urls refactored for toobar

--- base/urls.py
+++ base/urls.py
@@ -15,8 +15,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-
     urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include('debug_toolbar.urls')),
     ]
